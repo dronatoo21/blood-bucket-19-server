8cd1f9d2b01fef7e19905c0b9fc20a70123358fc
dronatoo <mrdronatoo@gmail.com>
2023-11-28 07:13:17 -0800

    created server side

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+require('dotenv').config()
+const cors = require('cors');
+const port = process.env.PORT || 4000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('server is runing')
+})
+
+app.listen(port, () => {
+    console.log(`server listening on port ${port}`);
+})
