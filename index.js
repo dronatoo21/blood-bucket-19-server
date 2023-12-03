abe136832e92133a7f0210e98ce9cd5f97fe94ac
dronatoo <mrdronatoo@gmail.com>
2023-12-03 12:44:57 -0800

    created methods for blog page

--- index.js
+++ index.js
@@ -62,6 +62,39 @@ async function run() {
         const result = await blogCollection.find().toArray();
         res.send(result); 
       })
+      app.get('/blog', async (req, res) => {
+        const result = await blogCollection.find().toArray();
+        res.send(result); 
+      })
+
+      app.delete('/blogs/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)}
+        const result = await blogCollection.deleteOne(query);
+        res.send(result)
+      })
+
+      app.patch('/blogs/:id', async (req, res) => {
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)}
+        const updatedDoc = {
+          $set: {
+            blogStatus: 'published',
+          }}
+        const result = await blogCollection.updateOne(filter, updatedDoc)
+        res.send(result)
+      })
+      
+      app.patch('/blog/:id', async (req, res) => {
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)}
+        const updatedDoc = {
+          $set: {
+            blogStatus: 'draft',
+          }}
+        const result = await blogCollection.updateOne(filter, updatedDoc)
+        res.send(result)
+      })
 
 
       // --------------------------------
